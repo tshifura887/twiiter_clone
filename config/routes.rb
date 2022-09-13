1610cc94fc4353793c8919bfb61794123fe21abd
Talifhani Tshifura <tshifura887@gmail.com>
2022-09-13 14:40:50 +0200

    Sign Up user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
-
   root "main#index"
-  
+
+  get 'sign_up', to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 end
